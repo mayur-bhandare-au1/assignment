8c442f4d7e22040031ba1011ffa32501dd199d60
Mayur Bhandare <mayur.b@docvita.com>
2021-02-25 00:16:29 +0530

    code refactor

--- dashboard.php
+++ dashboard.php
@@ -2,22 +2,24 @@
 session_start();
 include 'config.php';
 $conn = DB();
-if(!isset($_SESSION['userid'])){
-           header("Location:index.php");                     
-                                }
+if (!isset($_SESSION['userid']))
+{
+    header("Location:index.php");
+}
 
-  else{
-       $userid = $_SESSION['userid'];
-       $stmt = $conn->prepare("SELECT * FROM usermaster WHERE userid=:userid");
+else
+{
+    $userid = $_SESSION['userid'];
+    $stmt = $conn->prepare("SELECT * FROM usermaster WHERE userid=:userid");
     $stmt->bindParam("userid", $userid, PDO::PARAM_STR);
     $stmt->execute();
-     while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
     {
         extract($row);
         // echo $username;
         
-    } 
-    }                           
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -36,26 +38,34 @@ if(!isset($_SESSION['userid'])){
     <div class="row">
         <div class="col-md-4">
             <div class="list-group">
-                <?php if($utype == 0) { ?>
+                <?php if ($utype == 0)
+{ ?>
  <a href="#" class="list-group-item list-group-item-action">
     My Account
   </a>
   <a href="#" class="list-group-item list-group-item-action">MIS</a>
-                                     <?php } elseif($utype == 1) {?>
+                                     <?php
+}
+elseif ($utype == 1)
+{ ?>
 
 <a href="#" class="list-group-item list-group-item-action">
     User Management
   </a>
   <a href="#" class="list-group-item list-group-item-action">Recharges</a>
   <a href="#" class="list-group-item list-group-item-action">Mis</a>
-                                                                  <?php } else {?>
+                                                                  <?php
+}
+else
+{ ?>
   <a href="#" class="list-group-item list-group-item-action">
     User Management
   </a>
   <a href="#" class="list-group-item list-group-item-action">Admin Management</a>
   <a href="#" class="list-group-item list-group-item-action">Recharges</a>
   <a href="#" class="list-group-item list-group-item-action">Mis</a>
- <?php } ?>
+ <?php
+} ?>
 </div>
         </div>
         <div class="col-md-8">
