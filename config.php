766bfb7c3981c6e4bbd294df530350a07eb64148
Mayur Bhandare <mayur.b@docvita.com>
2021-02-24 20:15:56 +0530

    Mysql Config Added

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+// database Connection variables
+define('HOST', 'localhost'); // Database host name ex. localhost
+define('USER', 'root'); // Database user. ex. root ( if your on local server)
+define('PASSWORD', ''); // user password  (if password is not set for user then keep it empty )
+define('DATABASE', 'assignment'); // Database Database name
+ 
+function DB()
+{
+    try {
+        $db = new PDO('mysql:host='.HOST.';dbname='.DATABASE.'', USER, PASSWORD);
+        return $db;
+    } catch (PDOException $e) {
+        return "Error!: " . $e->getMessage();
+        die();
+    }
+}
+?>
